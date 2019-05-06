5ec7e48904b74ee2213e2c314ae2627108febf0c
zefaxet <zefaxet@gmail.com>
2019-05-06 16:47:27 -0500

    WIP cells and grid

--- c/anttype.h
+++ c/anttype.h
@@ -6,25 +6,35 @@
 typedef int * position_t;
 
 //Two degrees of expressability, generally rotation for 3 dimensional motion
-typedef struct orientation_t {
+typedef struct orientation {
 	
 	int x;
 	int y;
 	
 } orientation_t;
 
-//Should represent a single ant on 2D or 3D tape
-typedef struct turmite_t {
+//Should represent a single ant on 2D or 3D grid
+typedef struct turmite {
 	
 	position_t position;
 	orientation_t orientation;
 	
 } turmite_t;
 
-typedef struct grid_t {
+typedef struct cell {
+
+	void (* rule)(struct cell *, turmite_t *);
+	int state;
+
+} cell_t;
+
+typedef struct grid {
 	
+	turmite_t * turmites;
+	cell_t ** cells;
 	
-	
-}
+} grid_t;
 
 orientation_t generateOrientation(int x, int y);
+
+//TODO Function to create cell with some initial state, and determine way to locate cell in grid
