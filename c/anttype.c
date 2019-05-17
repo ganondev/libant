10249d47feb70baedc95869ed49631574230c30a
zefaxet <zefaxet@gmail.com>
2019-05-17 16:50:00 -0500

    Removed unneeded types

--- c/anttype.c
+++ c/anttype.c
@@ -1,25 +1,14 @@
 #include "anttype.h"
 #include "string.h"
 
-ant_t * zero_ant_position(ant_t * ant, size_t size, bool allocated)
+ant_t * zero_ant_position(ant_t * ant, size_t size)
 {
 	
 	ant = realloc(ant, sizeof(ant_t) + size * sizeof(long long int));
-	memset(ant->pos, 0, size * sizeof(long long int));
+	memset(ant->position, 0, size * sizeof(long long int));
 	ant->tuple_size = size;
 	
 	return ant;
-	
-}
-
-ant_orientation_t generate_orientation(int x, int y)
-{
-	
-	ant_orientation_t orient;
-	orient.x = x;
-	orient.y = y;
-
-	return orient;
 	
 }
 
--- c/anttype.h
+++ c/anttype.h
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//variable sized integer array
-//TODO redundant?
-typedef int * ant_position_t;
-
-//Two degrees of expressability, generally rotation for 3 dimensional motion
-typedef struct ant_orientation_t ant_orientation_t;
-
 //Should represent a single ant on 2D or 3D grid
 typedef struct ant_t ant_t;
 
@@ -15,36 +8,22 @@ typedef struct ant_cell_t ant_cell_t;
 
 typedef struct ant_grid_t ant_grid_t;
 
-struct ant_orientation_t
-{
-
-	int x;
-	int y;
-
-};
-
 struct ant_t
 {
 
-	ant_orientation_t orientation;
-	ant_position_t position;
+	int orientation;
 	size_t tuple_size;
-	long long int pos[];
+	long long int position[];
 
 };
 
 struct ant_cell_t
 {
 
-	ant_position_t position;
+	//TODO Same as ant_t: C99 VLA? 
+	long long int * position;
 	void (* rule)(ant_cell_t *, ant_t *);
 	int state;
-
-	//TODO array
-	ant_cell_t * north;
-	ant_cell_t * south;
-	ant_cell_t * east;
-	ant_cell_t * west;
 
 };
 
@@ -56,11 +35,7 @@ struct ant_grid_t
 
 };
 
-ant_t * zero_ant_position(ant_t *, size_t, bool);
-
-size_t ant_position_tuple_size(ant_t *);
-
-ant_orientation_t generate_orientation(int x, int y);
+ant_t * zero_ant_position(ant_t *, size_t);
 
 ant_grid_t new_grid(ant_cell_t * origin);
 
