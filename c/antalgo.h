f0bac512810e2e4973b7bcde8f07ab0fe91f05e1
zefaxet <zefaxet@gmail.com>
2019-05-22 11:41:30 -0500

    2d cardinal constants

--- c/antalgo.h
+++ c/antalgo.h
@@ -1,7 +1,17 @@
+#ifndef LIBANT_ANTALGO_H
+#define LIBANT_ANTALGO_H
+
 #include "anttype.h"
+
+static const int NORTH_2D[2] = {0, -1};
+static const int SOUTH_2D[2] = {0, 1};
+static const int WEST_2D[2]= {-1, 0};
+static const int EAST_2D[2]= {1, 0};
 
 // general functions for this library's automaton
 void turmite_update(ant_t * ant);
-void next_state_create(grid_t * grid);
+void next_state_create(ant_grid_t * grid);
 
-grid_t langtons_ant(/* TODO initial state */);
+ant_grid_t langtons_ant(/* TODO initial state */);
+
+#endif
