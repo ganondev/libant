7827a11b56189e564f425dd96c7527250b58d0e6
zefaxet <zefaxet@gmail.com>
2019-05-23 17:14:32 -0500

    Working on langton's ant defaults

--- c/antalgo.c
+++ c/antalgo.c
@@ -1,16 +1,33 @@
 #include "antalgo.h"
 
-void next_state_create(grid_t * grid)
+// void next_state_create(grid_t * grid)
+// {
+
+    // int ant_count = sizeof(grid->turmites);
+    // for (int i = 0; i < ant_count; i++)
+    // {
+
+        // turmite_t * ant = grid->turmites[i];
+        // cell_t * ant_cell = ant->position;
+        // ant_cell->rule(ant_cell, ant);
+
+    // }
+
+// }
+
+void langtons_ant_default_directive(ant_t * ant)
 {
+	
+	ant->position[0] += CARDINAL_2D[ant->orientation][0];
+	ant->position[1] += CARDINAL_2D[ant->orientation][1];
+	
+}
 
-    int ant_count = sizeof(grid->turmites);
-    for (int i = 0; i < ant_count; i++)
-    {
-
-        turmite_t * ant = grid->turmites[i];
-        cell_t * ant_cell = ant->position;
-        ant_cell->rule(ant_cell, ant);
-
-    }
-
+ant_t * create_langtons_ant()
+{
+	
+	ant_t * ant = create_ant(2);
+	ant->directive = langtons_ant_default_directive;
+	return ant;
+	
 }
--- c/antalgo.h
+++ c/antalgo.h
@@ -4,14 +4,20 @@
 #include "anttype.h"
 
 static const int NORTH_2D[2] = {0, -1};
-static const int SOUTH_2D[2] = {0, 1};
-static const int WEST_2D[2]= {-1, 0};
 static const int EAST_2D[2]= {1, 0};
+static const int SOUTH_2D[2] = {0, 1};
+static const int WEST_2D[2] = {-1, 0};
+
+static const int * CARDINAL_2D[4] = {NORTH_2D, EAST_2D, SOUTH_2D, WEST_2D};
 
 // general functions for this library's automaton
 void turmite_update(ant_t * ant);
 void next_state_create(ant_grid_t * grid);
 
 ant_grid_t langtons_ant(/* TODO initial state */);
+
+void langtons_ant_default_directive(ant_t *);
+
+ant_t * create_langtons_ant();
 
 #endif
