8e9d97df9e87e5696ad89acced993c25e5f26f51
zefaxet <zefaxet@LAPTOP-FLLEI7HD.localdomain>
2019-05-31 09:37:30 -0500

    Hashtable compiles with msvc

--- py/setup.py
+++ py/setup.py
@@ -5,10 +5,14 @@ sources = [	r'./src/libant_py_ant.c',
 			r'./src/py_ant2.c',
 			r'./src/py_ant3.c',
 			r'./src/py_langtons_ant.c',
-			r'../c/anttype.c',
-			r'../c/antalgo.c' ]
+			r'../c/src/anttype.c',
+			r'../c/src/antalgo.c',
+			r'../dependencies/libdict/src/dict.c',
+			r'../dependencies/libdict/src/hashtable_common.c',
+			r'../dependencies/libdict/src/hashtable.c',
+			r'../dependencies/libdict/src/hashtable2.c', ]
 
-includes = [r'./include/', r'../c/']
+includes = [r'./include/', r'../c/include', r'../dependencies/libdict/include', r'../dependencies/libdict/src', ]
 
 setup(name='libant',
 		version='0.1',
