a3d88fbc70fb41d0b92571479bd1c558a99c0b99
zefaxet <zefaxet@gmail.com>
2019-10-02 01:56:35 -0500

    Extra ansi codes

--- c/include/antmacro.h
+++ c/include/antmacro.h
@@ -2,9 +2,18 @@
 #define ANTMACRO_H
 
 #ifdef LIBANT_DEBUG
+
 #define ANSI_RED "\033[31;1;4m"
+#define ANSI_BLUE "\033[34;1;1m"
+#define ANSI_YELLOW "\033[33;1;1m"
+#define ANSI_GREEN "\033[92;1;1m"
 #define ANSI_END "\033[0m"
-#define DEBUG(tok) ANSI_RED tok ANSI_END
+
+#define ERROR(tok) ANSI_RED tok ANSI_END
+#define TRACE(tok) ANSI_BLUE tok ANSI_END
+#define DEBUG(tok) ANSI_YELLOW tok ANSI_END
+#define IO_OK(tok) ANSI_GREEN tok ANSI_END
+
 #endif
 
 #define str(x) #x
