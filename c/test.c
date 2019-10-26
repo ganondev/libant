1a0b06d6f056682d3b9d0e90625a993746b0d57a
нет <naw@fam>
2019-10-25 19:10:52 -0500

    Working on basic test framework to make it easier to track broken tests

--- c/test.c
+++ c/test.c
@@ -1,21 +1,64 @@
 #define LIBANT_DEBUG
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
 
 #include <libant.h>
+
+#ifndef _WIN32
+
+#include <mcheck.h>
+
+#else
+
+#define mprobe(arg) 0
+#define MCHECK_OK 0
+
+#endif
+
+#define mprobe_assert(arg) assert(mprobe(arg)==MCHECK_OK)
+
+#define test(NAME)	n_tests++; \
+					test_names = realloc(test_names, sizeof(char *) * n_tests); \
+					test_names[n_tests - 1] = #NAME; \
+					tests = realloc(tests, sizeof(test_fn) * n_tests); \
+					tests[n_tests - 1] = NAME;
+
+typedef int(*test_fn)();
+
+int n_tests = 0;
+int i = 0;
+char ** test_names = NULL;
+test_fn * tests = NULL;
+
+void pp_puts(const char* msg)
+{
+
+	printf("%s\n%d/%d\r", msg, i, n_tests);
+
+}
+
+void pp_puts_empty()
+{
+
+	printf("%d/%d\r", i, n_tests);
+
+}
 
 int grid_create()
 {
 
-	puts(TRACE("Beginning grid head creation test."));
+	pp_puts(TRACE("Beginning grid head creation test."));
 
 	ant_grid_t grid = new_grid(NULL, NULL);
 	assert(grid.get == NULL);
 	assert(grid.insert == NULL);
-	assert(grid.scan_list_size = 0);
+	assert(grid.scan_list_size == 0);
 
-	puts(IO_OK("Grid head creation test complete!"));
+	mprobe_assert(grid.scan_list);
+
+	pp_puts(IO_OK("Grid head creation test complete!"));
 	return 1;
 
 }
@@ -23,8 +66,19 @@ int grid_create()
 int main()
 {
 
+	test(grid_create);
+
 	puts(TRACE("\nBeginning tests for libant core library.\n"));
-	grid_create();
+
+	pp_puts_empty();
+	while (i < n_tests)
+	{
+
+		tests[i++]();
+		pp_puts_empty();
+
+	}
+
 	puts(IO_OK("\nCompleted all tests for libant core library.\n"));
 
 }
