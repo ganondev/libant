9a94d5eceb955ff22f3a4af1a09c89ee4b4b81da
zefaxet <zefaxet@gmail.com>
2019-11-01 01:58:58 -0500

    Small example with triangle grid

--- py/example/triangles.py
+++ py/example/triangles.py
@@ -1,19 +1,25 @@
 from sys import argv
+from math import ceil
 gui = not "blind" in argv
 if gui:
 	import pygame
 import libant
 
 SCREEN_SIZE = 700
-STAGE_SIZE = 175  # 175 is largest size without bezels for 700 x 700 window
-DIRECTIONS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+STAGE_SIZE_X = 174
+STAGE_SIZE_Y = ceil(STAGE_SIZE_X / 2)
 
-
-def draw_bordered_square(x, y, filled, size):
+def draw_bordered_triangle(x, y, filled, size, color=None):
 	if gui:
-		pygame.draw.rect(screen, (0, 0, 0), (x, y, size, size)), filled
-		if not filled:
-			pygame.draw.rect(screen, (255, 255, 255), (x + 1, y + 1, size - 2, size - 2))
+		up = (x % 2) ^ (y % 2)
+		x = x * sizeof_rect / 2
+		y = y * sizeof_rect
+		vertices = [(x, y), (x + size, y), (x + size / 2, y + size)] \
+			if up else [(x, y + size), (x + size, y + size), (x + size / 2, y)]
+		pygame.draw.polygon(screen, (255, 255, 255), vertices)
+		pygame.draw.polygon(screen, (0, 0, 0), vertices, int(not filled))
+		if color:
+			pygame.draw.polygon(screen, color, vertices)
 
 
 def grid_to_screen(x, y):
@@ -26,14 +32,14 @@ if gui:
 
 	screen = pygame.display.get_surface()
 
-sizeof_rect = int(SCREEN_SIZE / STAGE_SIZE)
-bezel = int((SCREEN_SIZE - (STAGE_SIZE * sizeof_rect)) / 2)
+sizeof_rect = int(SCREEN_SIZE / (STAGE_SIZE_X / 2))
 cells = libant.CartesianGrid()
-for x in range(bezel, STAGE_SIZE * sizeof_rect + bezel, sizeof_rect):
-	for y in range(bezel, STAGE_SIZE * sizeof_rect + bezel, sizeof_rect):
-		draw_bordered_square(x, y, False, sizeof_rect)
+for x in range(STAGE_SIZE_X):
+	for y in range(STAGE_SIZE_Y):
+		draw_bordered_triangle(x, y, False, sizeof_rect)
 
-x_pos = y_pos = int(STAGE_SIZE / 2)
+x_pos = int(STAGE_SIZE_X / 2)
+y_pos = int(STAGE_SIZE_Y / 2)
 cells.insert(x_pos, y_pos, True)
 ant = libant.LangtonsAnt()
 ant.position = (x_pos, y_pos)
@@ -49,19 +55,19 @@ while True:
 				pause = not pause
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				x, y = pygame.mouse.get_pos()
-				x = int(x / int(SCREEN_SIZE / STAGE_SIZE))
-				y = int(y / int(SCREEN_SIZE / STAGE_SIZE))
+				x = int(x / int(SCREEN_SIZE / STAGE_SIZE_X))
+				y = int(y / int(SCREEN_SIZE / STAGE_SIZE_X))
 				flip_cell(cells.get(*grid_to_screen(x, y)))
 				print(x, y)
 			if event.type == pygame.QUIT:
 				exit(0)
 	if not pause:
 		cell_value = cells.get(ant.x, ant.y)
-		draw_bordered_square(*grid_to_screen(ant.x, ant.y), not cell_value, sizeof_rect)
+		draw_bordered_triangle(ant.x, ant.y, not cell_value, sizeof_rect)
 		cells.tick()
-		ant.x %= STAGE_SIZE 
-		ant.y %= STAGE_SIZE
+		ant.x %= STAGE_SIZE_X 
+		ant.y %= STAGE_SIZE_Y
 		if gui:
-			pygame.draw.rect(screen, (255, 0, 0), (*map(lambda i: i, grid_to_screen(ant.x, ant.y)), sizeof_rect - 1, sizeof_rect - 1))
+			draw_bordered_triangle(ant.x, ant.y, True, sizeof_rect, (255, 0, 0))
 	if gui:
 		pygame.display.flip()
