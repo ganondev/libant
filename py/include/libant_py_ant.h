89a559a3d9718333156884124dea5a8daa50e7e2
zefaxet <zefaxet@AutoThotPatrol.localdomain>
2019-11-06 02:20:48 -0600

    Cleanup py_ant header

--- py/include/libant_py_ant.h
+++ py/include/libant_py_ant.h
@@ -16,45 +16,31 @@ typedef struct
 
 	PyObject_HEAD
 	ant_t * ant;
-	PyObject * py_directive;
+	PyFunctionObject * py_directive; //TODO PyMethodObject
 
 } py_ant;
 
-PyObject * position_as_py_long(ant_t *, int); //todo inline
+inline PyObject * position_as_py_long(ant_t * ant, int index)
+{
+	
+	return PyLong_FromLongLong(ant->position[index]);
+	
+}
+
+void ant_t_directive_wrapper(ant_t *, ant_grid_t *);
 
 PyObject * langtons_ant_default_directive_wrapper(PyObject * module, PyObject * arg);
 
 PyMethodDef langtons_ant_directive;
 
-extern PyObject * langtons_ant_directive_func;
+extern PyFunctionObject * langtons_ant_directive_func;
 
 PyTupleObject * ant_get_position(py_ant *, void *);
 
-PyLongObject * ant_get_orientation(py_ant *, void *);
-
-PyObject * ant_get_directive(py_ant *, void *);
-
 int ant_set_position(py_ant *, PyTupleObject *, void *);
-
-int ant_set_orientation(py_ant *, PyObject *, void *);
-
-int ant_set_directive(py_ant *, PyObject *, void *);
-
-PyObject * ant_new(PyTypeObject *, PyObject *, PyObject *);
 
 int ant_init(py_ant *, PyObject *, PyObject *);
 
-void ant_dealloc(py_ant *);
-
-#ifndef _WIN32
-static
-#endif
-PyGetSetDef ant_getsetters[];
-
 PyTypeObject py_ant_type;
-
-PyObject * langtons_ant_default_directive_wrapper(PyObject * module, PyObject * arg);
-
-PyMethodDef langtons_ant_directive;
 
 #endif
