190406660b428f1a2aca98a1816f1364d296655a
zefaxet <zefaxet@gmail.com>
2019-11-12 11:29:07 -0600

    Proved new way to express debugging statements. Will proceed to fully implement as debug statements are encountered

--- c/src/grid.c
+++ c/src/grid.c
@@ -3,13 +3,9 @@
 void grid_add_ant(ant_grid_t * grid, ant_t * ant)
 {
 
-	#ifdef LIBANT_DEBUG
-	printf(TRACELN("Adding ant to scan list with initial size %zu."), grid->scan_list_size);
-	#endif
+	LOG(printf(TRACELN("Adding ant to scan list with initial size %zu."), grid->scan_list_size));
 	grid->scan_list = realloc(grid->scan_list, sizeof(ant_t *) * ++(grid->scan_list_size)); // TODO find alternative, this is unsafe
-	#ifdef LIBANT_DEBUG
-	printf(DEBUGLN("Scan list new size: %zu."), grid->scan_list_size);
-	#endif
+	LOG(printf(DEBUGLN("Scan list new size: %zu."), grid->scan_list_size));
 	grid->scan_list[grid->scan_list_size - 1] = ant;
 
 }
@@ -17,9 +13,7 @@ void grid_add_ant(ant_grid_t * grid, ant_t * ant)
 ant_grid_t new_grid(grid_getfn getfn, grid_insertfn insertfn)
 {
 
-	#ifdef LIBANT_DEBUG
-	puts(TRACE("Creating new grid."));
-	#endif
+	LOG(puts(TRACE("Creating new grid.")));
 
 	ant_grid_t grid =
 	{
