ab6b611cc82758da5784967baa44618bd940171d
EJ <me@me>
2023-09-10 16:50:20 -0400

    Delete the rest

--- c/include/standards.h
+++
@@ -1,5 +0,0 @@
-#ifndef __libant_standards
-#define __libant_standards
-#define INT long long int
-#define UINT unsigned INT
-#endif
--- winbuild.bat
+++
@@ -1,5 +0,0 @@
-pushd C:
-call "C:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat"
-popd
-cl /LD c\antalgo.c c\anttype.c -Ic
-exit %errorlevel%
