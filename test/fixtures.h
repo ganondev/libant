73ad3b093cdd757b31de6639182e1cba43fd62ef
EJ <me@me>
2023-09-23 18:01:59 -0400

    Backend parameterization fixture

---
+++ test/fixtures.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <gtest/gtest.h>
+#include "../libant.h"
+
+template<coords Bounds={0, 0}>
+class AutomatonTestFixtures : public ::testing::TestWithParam<const char *> {
+public:
+    la_spatial_structure * backend;
+
+    static std::vector<const char *> sharedParams() {
+        return {
+            "la_matrix",
+            "la_quadtree"
+            // Add other backend types here as string identifiers
+        };
+    }
+
+    void SetUp() override {
+        std::string type = GetParam();
+        if (type == "la_matrix") {
+            backend = new la_matrix(Bounds);
+        } else if (type == "la_quadtree") {
+            backend = new la_quadtree(Bounds);
+        }
+        // Add other backend types here
+    }
+
+    void TearDown() override {
+        delete backend;
+    }
+};
+
+template<typename TestClass>
+std::string customTestName(const ::testing::TestParamInfo<typename TestClass::ParamType>& info) {
+    std::string type = info.param;  // Assuming ParamType is const char*
+    return type;  // Replace special characters if needed
+}
