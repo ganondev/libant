c1f3925c07942299fe09c510e9dd27b8556580e6
EJ <me@me>
2023-09-24 14:41:31 -0400

    Refactor main header

--- libant.h
+++ libant.h
@@ -1,10 +1,4 @@
 ﻿#pragma once
 #include "automaton/automaton.h"
 
-#include "spatial_structure/la_cell.h"
-#include "spatial_structure/la_spatial_structure.h"
-
-#include "spatial_structure/quadtree/la_quadtree.h"
-#include "spatial_structure/quadtree/la_node.h"
-
-#include "spatial_structure/la_matrix.h"
+#include "spatial_structure/spatial_structure.h"
