8d483d1a2057d7a704a56ab3bc61383b3a5fc686
EJ <me@me>
2023-09-30 01:19:22 -0400

    Rule structure

---
+++ rule/la_rule.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+#include <functional>
+#include <unordered_map>
+#include <variant>
+
+struct coords;
+
+namespace libant
+{
+    struct condition {
+        coords relative_position;
+        std::function<bool(int)> predicate;
+    };
+
+    struct assignment {
+        coords relative_position;
+        char variable_name;
+    };
+
+    struct invalid_variable_exception final : std::exception {
+        char name;
+        std::string message; // Store the message as a member variable
+
+        explicit invalid_variable_exception(const char name):
+            name(name),
+            message("Invalid variable name: " + std::string(1, name)) {}
+
+        [[nodiscard]]
+        const char * what() const noexcept override {
+            return message.c_str();
+        }
+    };
+
+    struct symbols {
+        std::unordered_map<char, int> table;
+        explicit symbols(const std::unordered_map<char, int>& values) : table(values) {}
+
+        [[nodiscard]]
+        int value_of(const char& key) const
+        {
+            if (const auto it = table.find(key); it != table.end()) {
+                return it->second;
+            }
+            throw invalid_variable_exception(key);
+        }
+    };
+    
+    struct rule {
+        std::vector<condition> conditions;
+        std::vector<assignment> assignments;
+        std::function<int(const symbols&)> mapper;
+
+        rule& cell_matches_scalar(const coords relative_coords, int scalar) {
+            const std::function predicate = [scalar](const int value) { return value == scalar; };
+            
+            return cell_satisfies_predicate(relative_coords, predicate);
+        }
+
+        rule& cell_satisfies_predicate(const coords relative_coords, const std::function<bool(int)>& predicate) {
+            conditions.push_back({relative_coords, predicate});
+            return *this;
+        }
+        
+        rule& variable_cell(const coords relative_coords, const char name) {
+            assignments.push_back({relative_coords, name});
+            return *this;
+        }
+
+        // TODO what happens if the variable doesn't exist?
+        rule& maps_to(std::function<int(const symbols&)> fn) {
+            mapper = std::move(fn);
+            return *this;
+        }
+        
+        [[nodiscard]] 
+        int apply(const std::vector<std::vector<int>>& slice) const {
+            std::unordered_map<char, int> variables;
+
+            // Validate conditions
+            for (const auto& [relative_position, predicate] : conditions) {
+                if (const int& cell_value = slice[relative_position.y][relative_position.x]; !predicate(cell_value)) {
+                    return 0; // Condition failed based on predicate
+                }
+            }
+
+            // Assign variables
+            for (const auto& [relative_position, variable_name] : assignments) {
+                const int& cell_value = slice[relative_position.y][relative_position.x];
+                variables[variable_name] = cell_value;
+            }
+
+            try
+            {
+                // Call the mapper function
+                return mapper(symbols{variables});
+            }
+            catch (const invalid_variable_exception& e)
+            {
+                std::cerr << "Encountered exception in application of rule: " << e.what() << std::endl;
+                return 0;
+            }
+        }
+    };
+}
+
+// inline int apply_rule(const rule& rule, const coords& center_coord) {
+//     const rule::cell_map_t cell_slice = get_cells_around(center_coord);  // To be implemented in derived classes
+//     return rule.apply(cell_slice);
+// }
---
+++ test/test_rule.cpp
@@ -0,0 +1,88 @@
+﻿#include <gtest/gtest.h>
+
+#include "../libant.h"
+
+using namespace libant;
+
+TEST(rule, test_rule_mapper) {
+    
+    rule r;
+
+    ASSERT_EQ(r.mapper, nullptr); // Mapper should be zeroed
+    
+    r.maps_to([](const symbols& vars) {
+        return 1;
+    });
+
+    ASSERT_NE(r.mapper, nullptr);  // Assert that mapper is set
+    ASSERT_EQ(r.apply({{}}), 1);  // Assert that apply returns the expected result using the variable value
+    
+}
+
+TEST(rule, test_rule_mapper_with_variable_cell) {
+    
+    rule r;
+    r.maps_to([](const symbols& vars) {
+        
+        // vars should be congruent with the assignments vector
+        EXPECT_EQ(vars.table.size(), 1);
+        EXPECT_TRUE(vars.table.contains('a'));
+        EXPECT_EQ(vars.value_of('a'), 1);
+        
+        return vars.value_of('a') + 1;
+        
+    });
+    
+    const std::vector<std::vector<int>> slice{{1}};
+    r.variable_cell({0, 0}, 'a');
+
+    ASSERT_EQ(r.assignments.size(), 1);  // Assert vector is populated
+    ASSERT_EQ(r.assignments[0].variable_name, 'a');  // Assert correct symbol
+    ASSERT_EQ(r.apply(slice), 2);  // Assert that apply returns the expected result using the variable value
+    
+}
+
+TEST(rule, test_rule_mapper_invalid_variable)
+{
+    
+    const std::function predicate = [](const symbols& vars) {
+        EXPECT_EQ(vars.table.size(), 0);
+        return vars.value_of('a') + 1; // should throw here
+    };
+    EXPECT_THROW(predicate(symbols{{}}), invalid_variable_exception);
+    
+    rule r;
+    r.maps_to(predicate);
+    EXPECT_EQ(r.apply({{}}), 0); // Apply should return the fallback value after catching exception
+    
+}
+
+TEST(rule, test_rule_predicate_condition) {
+    
+    rule r;
+    r.maps_to([](const symbols& vars) {
+        return 1;
+    });
+    r.cell_satisfies_predicate({0, 0}, [](const int val) { return val > 5; });
+
+    ASSERT_EQ(r.conditions.size(), 1);  // Assert vector is populated
+    ASSERT_TRUE(r.conditions[0].predicate(6));  // Assert positive case
+    ASSERT_EQ(r.apply({{6}}), 1);  // Assert positive case for apply
+    ASSERT_EQ(r.apply({{4}}), 0);  // Assert negative case for apply
+    
+}
+
+TEST(rule, test_rule_scalar_condition) {
+    
+    rule r;
+    r.maps_to([](const symbols& vars) {
+        return 1;
+    });
+    r.cell_matches_scalar({0, 0}, 5);
+
+    ASSERT_EQ(r.conditions.size(), 1);  // Assert vector is populated
+    ASSERT_TRUE(r.conditions[0].predicate(5));  // Assert predicate checks correctly
+    ASSERT_EQ(r.apply({{5}}), 1);  // Assert positive case for apply
+    ASSERT_EQ(r.apply({{4}}), 0);  // Assert negative case for apply
+    
+}
